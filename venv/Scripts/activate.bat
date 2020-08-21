5b7bb3726ce0352d850b903bc94b7c5fc01960c5
Andrei486 <apopescu486@gmail.com>
2020-08-21 12:09:11 -0400

    Test to get all courses
    Began implementing a script to retrieve ALL possible courses for a given term. Uses multiprocessing.
    Added support (untested) for courses without a scheduled time.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\Andrei\Minor_Projects\Schedule_Generator\venv
+set VIRTUAL_ENV=C:\Users\apope\Documents\GitHub\Schedule_Generator\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
